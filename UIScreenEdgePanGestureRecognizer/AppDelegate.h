66db33886ceb38ce3d467f84a2dbdde1e89b7e9f
je_ffy <jzxiaobao@gmail.com>
2015-08-16 16:35:59 +0800

    Initial Commit

---
+++ UIScreenEdgePanGestureRecognizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScreenEdgePanGestureRecognizer
+//
+//  Created by je_ffy on 15/8/16.
+//  Copyright (c) 2015年 je_ffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScreenEdgePanGestureRecognizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScreenEdgePanGestureRecognizer
+//
+//  Created by je_ffy on 15/8/16.
+//  Copyright (c) 2015年 je_ffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
